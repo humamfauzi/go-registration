969a75317a7779ba66f4b36593ca480de81a167c
Humam Fauzi <humam.fauzi.93@gmail.com>
2020-04-07 01:36:16 +0700

    add jwt for tokenization

--- auth.go
+++ auth.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"time"
+
+	"github.com/humamfauzi/go-registration/utils"
+	"github.com/lestrrat-go/jwx/jwa"
+	"github.com/lestrrat-go/jwx/jwt"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func GeneratePasswordHash(email string, password string) (string, error) {
+const (
+	ENCRYPTION_SALT         = "jh9J6nGvRyFznCjHJXgaLM"
+	JWT_SIGNATURE_ALGORITHM = jwa.HS256
+)
+
+func GeneratePasswordHash(email, password string) (string, error) {
 	combined := email + ":" + password
 	bytes, err := bcrypt.GenerateFromPassword([]byte(combined), bcrypt.DefaultCost)
 	return string(bytes), err
@@ -13,4 +25,26 @@ func GeneratePasswordHash(email string, password string) (string, error) {
 func ValidatePasswordHash(incoming, validator string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(validator), []byte(incoming))
 	return err == nil
+}
+
+func GenerateWebToken(Id string) ([]byte, error) {
+	token := jwt.New()
+	token.Set(`ID`, Id)
+	token.Set(`InternalToken`, utils.GenerateUUID("token", 4))
+	token.Set(`ValidUntil`, time.Now().Add(time.Hour*time.Duration(24)))
+	payload, err := token.Sign(JWT_SIGNATURE_ALGORITHM, []byte(ENCRYPTION_SALT))
+	if err != nil {
+		return []byte{}, err
+	}
+	return payload, nil
+}
+
+func ValidateWebToken(webToken []byte) bool {
+	options := jwt.WithVerify(JWT_SIGNATURE_ALGORITHM, []byte(ENCRYPTION_SALT))
+	token, err := jwt.Parse(bytes.NewReader(webToken), options)
+	if err != nil {
+		fmt.Printf("failed to parse JWT token: %s\n", err)
+		return false
+	}
+	return true
 }
--- auth_test.go
+++ auth_test.go
@@ -7,6 +7,31 @@ func TestGeneratePasswordHash(t *testing.T) {
 	t.Log(result)
 }
 
+func TestGenerateWebToken(t *testing.T) {
+	Id := "DummyId"
+	result, err := GenerateWebToken(Id)
+	if err != nil {
+		t.Error("should not error", err)
+		return
+	}
+	t.Log(string(result))
+}
+
+func TestValidateWebToken(t *testing.T) {
+	Id := "DummyId"
+	result, err := GenerateWebToken(Id)
+	if err != nil {
+		t.Error("should not error", err)
+		return
+	}
+	ok := ValidateWebToken(result)
+	if !ok {
+		t.Error("should not error", err)
+		return
+	}
+	return
+}
+
 func BenchmarkGeneratePasswordHash(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		GeneratePasswordHash("a@2.a", "secret")
