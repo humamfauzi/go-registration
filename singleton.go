f68e2020250b7e9eae1189b2cc158067ad355ea7
Humam Fauzi <humam.fauzi.93@gmail.com>
2020-04-09 00:24:58 +0700

    add logger (untested)

--- singleton.go
+++ singleton.go
@@ -1,21 +1,33 @@
-package registration
+package main
 
 import (
 	"context"
 
 	"github.com/gocql/gocql"
 	"github.com/humamfauzi/go-registration/exconn"
+	"github.com/humamfauzi/go-registration/utils"
 	"github.com/jinzhu/gorm"
 )
 
 var (
-	db         *gorm.DB
-	logger     *gocql.Session
-	documentDb context.Context
+	db            *gorm.DB
+	logStore      *gocql.Session
+	documentDb    context.Context
+	errorMap      map[string]string
+	loggerFactory utils.LoggerFactory
 )
 
 func InstantiateExternalConnection() {
 	db = exconn.ConnectToMySQL()
-	logger = exconn.ConnectToCassandra()
+	logStore = exconn.ConnectToCassandra()
 	documentDb = exconn.ConnectToMongo()
+	errorMap = utils.InitError("./error.json")
+	loggerFactory = InstantiateLoggerFactory(logStore)
+}
+
+func InstantiateLoggerFactory(logStore utils.LogStore) utils.LoggerFactory {
+	return utils.LoggerFactory{
+		LogList:    make(map[string]*Logger),
+		LogAddress: logStore,
+	}
 }
---
+++ utils/logger.go
@@ -0,0 +1,91 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type LoggerFactory struct {
+	LogList    map[string]*Logger
+	LogAddress LogStore
+}
+
+type LogStore interface {
+	SendLog(input []string) bool
+}
+
+func (lf *LoggerFactory) CreateLog() *Logger {
+	genUuid := uuid.New().String()
+	newLog := &Logger{
+		Factory: lf,
+		Id:      genUuid,
+	}
+	lf.LogList[genUuid] = newLog
+	return newLog
+
+}
+
+func (lf *LoggerFactory) WriteLog(loggerId string) {
+	sentLog := lf.LogList[loggerId].ToExternalFormat()
+	lf.LogAddress.SendLog(sentLog)
+}
+
+func (lf *LoggerFactory) DeleteLog(loggerId string) {
+	delete(lf.LogList, loggerId)
+}
+
+type Logger struct {
+	Factory      *LoggerFactory
+	Id           string
+	StartAt      time.Time
+	FinishAt     time.Time
+	Remarks      string
+	FunctionName string
+}
+
+func (l *Logger) ToExternalFormat() []string {
+	startAt, _ := l.StartAt.MarshalText()
+	finishtAt, _ := l.StartAt.MarshalText()
+	externalFormat := []string{
+		l.Id,
+		string(startAt),
+		string(finishtAt),
+		l.Remarks,
+		l.FunctionName,
+	}
+	return externalFormat
+}
+
+func (l *Logger) SetStartTime() *Logger {
+	l.StartAt = time.Now()
+	return l
+}
+
+func (l *Logger) SetFinishTime() *Logger {
+	l.FinishAt = time.Now()
+	return l
+}
+
+func (l *Logger) SetRemarks(remarks string) *Logger {
+	l.Remarks = remarks
+	return l
+}
+
+func (l *Logger) SetFunctionName(functionName string) *Logger {
+	l.FunctionName = functionName
+	return l
+}
+
+func (l *Logger) WriteLog() {
+	l.Factory.WriteLog(l.Id)
+}
+
+func (l *Logger) DeleteLog() {
+	l.Factory.DeleteLog(l.Id)
+}
+
+func (l *Logger) WriteAndDeleteLog() {
+	l.WriteLog()
+	l.DeleteLog()
+}
